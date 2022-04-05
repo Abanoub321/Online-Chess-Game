aa5df824f62c672648eae308aa5dc64332da7e23
Abanoub321 <abanoubmilad12@gmail.com>
2022-04-05 13:02:01 +0200

    Fix: Performance issues

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,12 @@ module.exports = {
     transform: {
       '^.+\\.(ts|tsx)?$': 'ts-jest',
       "^.+\\.(js|jsx)$": "babel-jest",
-    }
+    },
+    testEnvironment: 'node',
+    globals: {
+        'ts-jest': {
+            isolatedModules: true
+        }
+    },
   };
   
