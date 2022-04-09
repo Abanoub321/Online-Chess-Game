edcb928286e887319b91b66ce8ebf8674aaaa8f7
Abanoub321 <abanoubmilad12@gmail.com>
2022-04-09 20:35:04 +0200

    player

--- src/player/Player.ts
+++ src/player/Player.ts
@@ -1,9 +1,27 @@
+import Game from "../game/Game";
 
 
 export default class Player {
     name: string;
     color: string | undefined;
+    game: Game | undefined;
     constructor(name: string) {
         this.name = name;
+    }
+    createNewGame() {
+        if (this.game)
+            throw new Error('Player is already in a game');
+        this.game = new Game(this);
+    }
+    joinGame(game: Game) {
+        if (this.game)
+            throw new Error('Player is already in a game');
+
+        try {
+            game.joinGame(this);
+            this.game = game;
+        } catch (e: Error | any) {
+            throw e;
+        }
     }
 }
