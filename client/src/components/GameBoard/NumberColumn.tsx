99229b441de1d03120d8340adc7053fccc5729af
Abanoub321 <abanoubmilad12@gmail.com>
2022-05-07 19:10:29 +0200

    Fix: letters and number view

--- client/src/components/GameBoard/NumberColumn.tsx
+++ client/src/components/GameBoard/NumberColumn.tsx
@@ -3,7 +3,7 @@ export const NumberColumn = (props: any) => {
     const currentColor = props.currentColor;
     const renderColumn = () => {
         let column = [];
-        if (currentColor === 'white')
+        if (currentColor === 'black')
             for (let i = 0; i < 8; i++)
                 column.push(<h4 className="tile">{i + 1}</h4>)
         else
