3562ccca7dae9d7ef76b0fbdc0de3d5ca6b3264e
ahmed hesham <abanoubmilad12@gmail.com>
2023-02-25 17:05:36 +0200

    Fix: .env react

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,4 +1,6 @@
 import io from "socket.io-client";
+const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
+console.log("BASE_URL", BASE_URL);
 
-export const socket = io('http://localhost:5000');
+export const socket = io(BASE_URL);
